69382f682fd14de19d4630de52198d5c73b3ce56
Nutt Chairatana <nuttchai@outlook.com>
2023-05-05 01:07:03 +0700

    Update User Entity

--- user/domain/entities/user.entity.go
+++ user/domain/entities/user.entity.go
@@ -1,13 +1,28 @@
 package entity
 
 import (
+	"regexp"
+
 	value_object "github.com/nuttchai/go-ddd/user/domain/value-objects"
 )
 
 type User struct {
-	Id        int                  `json:"id"`
+	Id        string               `json:"id"`
 	FirstName string               `json:"first_name"`
 	LastName  string               `json:"last_name"`
 	Email     string               `json:"email"`
 	Address   value_object.Address `json:"address"`
+}
+
+func (u *User) isEmailValid() bool {
+	emailRegex := `^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`
+	emailPattern := regexp.MustCompile(emailRegex)
+	return emailPattern.MatchString(u.Email)
+}
+
+func (u *User) IsUserValid() bool {
+	if u.FirstName == "" || u.LastName == "" || u.isEmailValid() {
+		return false
+	}
+	return true
 }
