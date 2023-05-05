f3bb703270bf7a1f0ba79011a627bb17c1d75680
Nutt Chairatana <nuttchai@outlook.com>
2023-05-06 03:04:52 +0700

    Implement User Application Data Mapper

---
+++ user/app/data-mappers/user.data-mapper.go
@@ -0,0 +1,39 @@
+package data_mapper
+
+import (
+	entity "github.com/nuttchai/go-ddd/user/domain/entities"
+	value_object "github.com/nuttchai/go-ddd/user/domain/value-objects"
+	dto "github.com/nuttchai/go-ddd/user/dtos"
+)
+
+type UserAppDataMapper struct{}
+
+func (m *UserAppDataMapper) ToDomainEntity(dalEntity *dto.UserDTO) (*entity.User, error) {
+	return &entity.User{
+		Id:        dalEntity.Id,
+		FirstName: dalEntity.FirstName,
+		LastName:  dalEntity.LastName,
+		Email:     dalEntity.Email,
+		Address: value_object.Address{
+			Street:  dalEntity.Address.Street,
+			City:    dalEntity.Address.City,
+			State:   dalEntity.Address.State,
+			ZipCode: dalEntity.Address.ZipCode,
+		},
+	}, nil
+}
+
+func (m *UserAppDataMapper) ToDalEntity(domainEntity *entity.User) (*dto.UserDTO, error) {
+	return &dto.UserDTO{
+		Id:        domainEntity.Id,
+		FirstName: domainEntity.FirstName,
+		LastName:  domainEntity.LastName,
+		Email:     domainEntity.Email,
+		Address: dto.AddressDTO{
+			Street:  domainEntity.Address.Street,
+			City:    domainEntity.Address.City,
+			State:   domainEntity.Address.State,
+			ZipCode: domainEntity.Address.ZipCode,
+		},
+	}, nil
+}
