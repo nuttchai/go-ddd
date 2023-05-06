7a8a5715d5c06e66c77dcf7d9e86a8bf1648c2a0
Nutt Chairatana <nuttchai@outlook.com>
2023-05-06 22:17:21 +0700

    Update Model

--- user/infrastructure/models/address.model.go
+++ user/infrastructure/models/address.model.go
@@ -1,8 +1,10 @@
 package model
 
 type Address struct {
+	Id      string `json:"id"`
 	Street  string `json:"street"`
 	City    string `json:"city"`
 	State   string `json:"state"`
 	ZipCode string `json:"zip_code"`
+	UserId  string `json:"user_id"`
 }
--- user/infrastructure/models/user.model.go
+++ user/infrastructure/models/user.model.go
@@ -5,5 +5,5 @@ type User struct {
 	FirstName string  `json:"first_name"`
 	LastName  string  `json:"last_name"`
 	Email     string  `json:"email"`
-	Address   Address `gorm:"embedded"`
+	Address   Address `json:"address"`
 }
