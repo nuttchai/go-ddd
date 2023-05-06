c5f8b037ac100a775aac76e24e3b6aed3b15e980
Nutt Chairatana <nuttchai@outlook.com>
2023-05-06 20:03:29 +0700

    Update User ENV

--- user/config/env.config.go
+++ user/config/env.config.go
@@ -15,17 +15,17 @@ func initEnv() error {
 		return err
 	}
 
-	envDir := env.GetEnv("ENV_PATH", envDefaultDir)
+	envDir := env.GetEnv("USER_ENV_PATH", envDefaultDir)
 	env.LoadEnv(envDir)
 
-	dbType := env.GetEnv("DB_TYPE", "postgres")
-	dbUser := env.GetEnv("APP_DB_USER", "postgres")
-	dbPass := env.GetEnv("APP_DB_PASS", "postgres")
-	dbHost := env.GetEnv("DB_HOST", "localhost")
-	dbPort := env.GetEnv("DB_PORT", "5432")
-	dbName := env.GetEnv("APP_DB_NAME", "test")
-	dbDriver := env.GetEnv("DB_DRIVER", "postgres")
-	port := env.GetEnv("APP_PORT", "8000")
+	dbType := env.GetEnv("USER_DB_TYPE", "postgres")
+	dbUser := env.GetEnv("USER_APP_DB_USER", "postgres")
+	dbPass := env.GetEnv("USER_APP_DB_PASS", "postgres")
+	dbHost := env.GetEnv("USER_DB_HOST", "localhost")
+	dbPort := env.GetEnv("USER_DB_PORT", "5432")
+	dbName := env.GetEnv("USER_APP_DB_NAME", "test")
+	dbDriver := env.GetEnv("USER_DB_DRIVER", "postgres")
+	port := env.GetEnv("USER_APP_PORT", "8000")
 	dbConnStr := fmt.Sprintf(
 		"%s://%s:%s@%s:%s/%s?sslmode=disable",
 		dbType,
@@ -38,15 +38,16 @@ func initEnv() error {
 
 	// Get Value from Command Line if Exist
 	var env, serverPort, dsn, driver string
-	flag.StringVar(&env, "env", appEnv, "Application Environment")
-	flag.StringVar(&serverPort, "port", port, "Server Listening Port")
-	flag.StringVar(&dsn, "dsn", dbConnStr, "Data Source Name")
-	flag.StringVar(&driver, "driver", dbDriver, "Database Driver")
+	flag.StringVar(&env, "userEnv", appEnv, "Application Environment")
+	flag.StringVar(&serverPort, "userPort", port, "Server Listening Port")
+	flag.StringVar(&dsn, "userDsn", dbConnStr, "Data Source Name")
+	flag.StringVar(&driver, "userDriver", dbDriver, "Database Driver")
 	flag.Parse()
 
 	// Set Value to AppConfig
 	AppConfig.SetENV(env)
 	AppConfig.SetRESTConfig(serverPort)
 	AppConfig.SetDBMetaData(dsn, driver)
+
 	return nil
 }
