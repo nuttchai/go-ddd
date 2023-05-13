99d436ab27692c66132ac87787e8f630a4136e34
Nutt Chairatana <nuttchai@outlook.com>
2023-05-14 05:13:42 +0700

    Remove Json Field

--- internal/infra/models/user.model.go
+++ internal/infra/models/user.model.go
@@ -8,13 +8,13 @@ import (
 
 type User struct {
 	gorm.Model
-	Id        string    `gorm:"primaryKey;column:id;" json:"id"`
-	FirstName string    `gorm:"type:varchar(100);size:100;not null;default:null;column:first_name;" json:"first_name"`
-	LastName  string    `gorm:"type:varchar(100);size:100;not null;default:null;column:last_name;" json:"last_name"`
-	Email     string    `gorm:"type:varchar(50);size:50;not null;default:null;column:email;" json:"email"`
-	CreatedAt time.Time `gorm:"autoCreateTime:true;column:created_at;" json:"created_at"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime:true;column:updated_at;" json:"updated_at"`
+	Id        string    `gorm:"primaryKey;column:id;"`
+	FirstName string    `gorm:"type:varchar(100);size:100;not null;default:null;column:first_name;"`
+	LastName  string    `gorm:"type:varchar(100);size:100;not null;default:null;column:last_name;"`
+	Email     string    `gorm:"type:varchar(50);size:50;not null;default:null;column:email;"`
+	CreatedAt time.Time `gorm:"autoCreateTime:true;column:created_at;"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime:true;column:updated_at;"`
 
+	Address   Address `gorm:"foreignkey:AddressId"`
 	AddressId string
-	Address   Address `gorm:"references:Id" json:"address"`
 }
