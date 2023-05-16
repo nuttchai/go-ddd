061b4a27e64fe4799e5a8ffb87df603cb15a84a8
Nutt Chairatana <nuttchai@outlook.com>
2023-05-17 02:26:57 +0700

    Hide DB Logger when Production

--- internal/http/client/config/db.config.go
+++ internal/http/client/config/db.config.go
@@ -7,11 +7,18 @@ import (
 	context "github.com/nuttchai/go-ddd/utils/context"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func initDB() error {
+	env := AppConfig.GetENV()
+	dbConfig := &gorm.Config{}
+	if env == "production" {
+		dbConfig.Logger = logger.Default.LogMode(logger.Silent)
+	}
+
 	dsn := AppConfig.GetDBConfig().GetDSN()
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), dbConfig)
 	if err != nil {
 		return err
 	}
