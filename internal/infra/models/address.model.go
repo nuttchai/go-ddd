ed043d495dc3748b3b437d3d7d59317c4fb7f794
Nutt Chairatana <nuttchai@outlook.com>
2023-05-17 04:57:46 +0700

    Change ID Field to Generate UUID

--- internal/infra/models/address.model.go
+++ internal/infra/models/address.model.go
@@ -6,7 +6,7 @@ import (
 
 type Address struct {
 	gorm.Model
-	ID      string `gorm:"primaryKey;column:id;" json:"id"`
+	ID      string `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
 	Street  string `gorm:"type:varchar(30);size:30;column:street;" json:"street"`
 	City    string `gorm:"type:varchar(30);size:30;column:city;" json:"city"`
 	State   string `gorm:"type:varchar(5);size:5;column:state;" json:"state"`
--- internal/infra/models/user.model.go
+++ internal/infra/models/user.model.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
 	gorm.Model
-	ID        string  `gorm:"primaryKey;column:id;"`
+	ID        string  `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
 	FirstName string  `gorm:"type:varchar(100);size:100;not null;default:null;column:first_name;"`
 	LastName  string  `gorm:"type:varchar(100);size:100;not null;default:null;column:last_name;"`
 	Email     string  `gorm:"type:varchar(50);size:50;not null;default:null;column:email;"`
